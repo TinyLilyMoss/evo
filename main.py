a57526de22f734357aaf818feb4141249a5c9cf0
TinyLilyMoss <LilyTaylorMurphy@gmail.com>
2021-12-05 13:44:04 -0500

    added elif's

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 import sys
 import random
 
-## bots up pygame... and then tells you that its been booted... 
+## boots up pygame... and then tells you that its been booted... 
 
 pygame.init()
 print("pygame has been booted")
@@ -44,11 +44,11 @@ while True:
         print("la2 has won!")
         win = 1
         a += 1
-    if la1 > la2:
+    elif la1 > la2:
         print("la1 has won")
         win = 0
         b += 1
-    if la1 == la2:
+    elif la1 == la2:
         print("must have been a draw")
         win = 2
         c += 1
@@ -59,11 +59,11 @@ while True:
     if win == 1:
         color = pygame.image.load("assets/CATEJAM.png") ## lol 
         screen.blit(color, (0, 0))
-    if win == 0:
+    elif win == 0:
         color = pygame.Surface((1000, 1000))
         color.fill((0, 0, 255))
         screen.blit(color, (0, 0))
-    if win == 2:
+    elif win == 2:
         color = pygame.Surface((1000, 1000))
         color.fill((0, 255, 0))
         screen.blit(color, (0, 0))
