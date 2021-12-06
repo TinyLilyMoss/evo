80ec8e39435fa1b2abeecdf5af26bcb88013a7bf
TinyLilyMoss <LilyTaylorMurphy@gmail.com>
2021-12-06 17:56:09 -0500

    deleted win generator and updated evosims name

--- main.py
+++ main.py
@@ -1,71 +1,42 @@
-## DAMN BITCH, IMPORT THOSE MODULES ',:|
-
 import pygame
 import sys
 import random
 
-## boots up pygame... and then tells you that its been booted... 
-
 pygame.init()
 print("pygame has been booted")
 
-## variables to get data on the RNG, don't put these in the while loop, it just resets them
+## the pygame parts are only here because eventually this will render things
+## maybe it can change color based on how close your get
 
-a = 0
-b = 0
-c = 0
+screen = pygame.display.set_mode((100, 100))
 
-## sets the display size
+pygame.display.set_caption('new version')
+clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((500, 500))
-
-pygame.display.set_caption('Evolution base') ## captions the window 
-clock = pygame.time.Clock() ## ???
+WORLDSPVALUE = (random.randint(0, 10))
+WORLDSTRVALUE = (random.randint(0, 10))
 
 while True:
-
-    ## allows you to quit the game
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
+    
+    speed = (random.randint(0, 10))
+    stre = (random.randint(0, 10))
 
-    ## the random number generator, it works by the power of magic and pure hormones
+    print (speed, stre, WORLDSPVALUE, WORLDSTRVALUE)
 
-    x = (random.randint(0, 1))
-    y = (random.randint(0, 1))
-    la1 = y
-    la2 = x
+    if speed + stre < WORLDSPVALUE + WORLDSTRVALUE:
+        print("lame guy")
+    if speed + stre > WORLDSPVALUE + WORLDSTRVALUE:
+        print("awesome guy")
+        break
+    if speed + stre == WORLDSPVALUE + WORLDSTRVALUE:
+        print("satisfactory guy")
+        break
 
-    ## this just decide who wins, or whether or not its a draw
+    pygame.display.update()
+    clock.tick(1)
 
-    if la1 < la2:
-        print("la2 has won!")
-        win = 1
-        a += 1
-    elif la1 > la2:
-        print("la1 has won")
-        win = 0
-        b += 1
-    elif la1 == la2:
-        print("must have been a draw")
-        win = 2
-        c += 1
-    print(a, b, c) ## prints the win to loss ratio, mostly just to test how random the RNG is? 
-
-    ## renders certain things based on who wins, 
-
-    if win == 1:
-        color = pygame.image.load("assets/CATEJAM.png") ## lol 
-        screen.blit(color, (0, 0))
-    elif win == 0:
-        color = pygame.Surface((1000, 1000))
-        color.fill((0, 0, 255))
-        screen.blit(color, (0, 0))
-    elif win == 2:
-        color = pygame.Surface((1000, 1000))
-        color.fill((0, 255, 0))
-        screen.blit(color, (0, 0))
-    pygame.display.update() # updates the display (it does this because I asked nicely)
-    clock.tick(1) ## tps, change this to slow down or speed up the RNG process
+print ("congrats on making a cool guy!")
