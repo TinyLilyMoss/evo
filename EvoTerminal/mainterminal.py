80c56a55538f269fbe9f508fe6749f5a61600808
TinyLilyMoss <LilyTaylorMurphy@gmail.com>
2021-12-19 12:37:40 -0500

    added comments, made foodneed var more intuitive

--- EvoTerminal/mainterminal.py
+++ EvoTerminal/mainterminal.py
@@ -1,6 +1,8 @@
 ## coming soon - [~] UI, [V] enviornment custimization, [] generations, [] more variables for evolving
 ## [] and a less data oriented form of displaying the outcome of events
-## data oriented outcomes will be toggleable in the finished UI
+## data oriented outcomes will be toggleable in the finished UI build 
+
+## imports 
 
 import pygame
 import sys
@@ -9,6 +11,7 @@ import random
 pygame.init()
 
 ## print command to avoid using in blocks
+
 def MSG():
     print ("_________________________________________________")
     print ("variable comparison")
@@ -31,16 +34,20 @@ def MSG2():
     print (f"the amount of creatures that are in your enviornment is {animal}")
     print ("_________________________________________________")
 
+## line print command to make the UI more readable 
+
 def line():
     print("_________________________________________________________________________________________")
 
 ## all enviornmental values
 
-CREATURESDEAD = 0
+CREATURESDEAD = 0 ## for later 
 x = []
 y = []
 animal = 0 
 line()
+
+## number of creatures allowed in the enviormnet is set here 
 
 animalRNG = int(input("""please enter the number of creatures allowed in your enviornment 
 between 0-100 you want to allow in your enviornment! type 999 for a random value! - """))
@@ -49,12 +56,16 @@ if animalRNG == 999:
 
 line()
 
+## value of speed in the enviornment 
+
 WORLDSPVALUE = int(input("""please enter the value of speed in your enviornment
 between 0-10! type 999 for a random value! - """))
 if WORLDSPVALUE == 999:
     WORLDSPVALUE = (random.randint(0, 10))
 
 line()
+
+## value of strength in the enviorment
 
 WORLDSTRVALUE = int(input("""please enter the value of strength in your enviornment
 between 0-10! type 999 for a random value! - """))
@@ -63,6 +74,8 @@ if WORLDSTRVALUE == 999:
 
 line()
 
+## amount of food in the enviorment 
+
 FOOD = int(input("""please enter the amount of food in your enviornment
 between 0-10! type 999 for a random value! - """))
 if FOOD == 999:
@@ -70,17 +83,25 @@ if FOOD == 999:
 
 line()
 
+## allowing or disallowing user input mid code
+
 constant = int(input("""if you would like to generate creatures with no pauses for user 
 input, type one, zero to stop for user input! - """))
 
 line()
 
+## tick speed when not given a variable from the user
+
 guy = 1
+
+## user input for TPS 
 
 if constant == 1: 
     guy = int(input("please type the ticks per second at which you want to generate creatures - "))
 
 line()
+
+## value for updating to the next generation 
 
 win = 0
 
@@ -104,9 +125,13 @@ while True:
 
     while win == 0:
 
+        ## sets the birth creatures values
+
         speed = (random.randint(0, 10))
         stre = (random.randint(0, 10))
-        foodneed = (random.randint(0, 10))
+        foodneed = (stre/ 2) + (speed / 2)
+
+        ## prints data values for studying/troubleshootiing
 
         MSG()
 
@@ -191,6 +216,9 @@ press enter to create another creature""")
 ## generation decent
 
     while win >= 1:
+
+        ## sets the decent creatures values
+
         x = speed
         x += (random.randint(-1, win))
         y = stre
@@ -198,6 +226,8 @@ press enter to create another creature""")
         if win > 5:
             win -= 2
         
+        ## data values for decent creatures
+
         MSG2()
         
         if y < WORLDSTRVALUE:
