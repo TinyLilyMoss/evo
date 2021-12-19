0d2cad7fe446a2b6d687bc1661d808431d896a14
TinyLilyMoss <LilyTaylorMurphy@gmail.com>
2021-12-19 11:40:42 -0500

    updated batch file

--- [extremely buggy] testing/pipinstall.bat
+++ [extremely buggy] testing/pipinstall.bat
@@ -1,5 +1,8 @@
 @echo off 
 rem this is a testing feature for windows machines for people to run this and install all needed modules
+echo press any button to install all necisary pip files, or exit the cmd to cancel
+pause 
 pip install pygame
 pause 
-exit
+echo that should have worked :) 
+exit 
